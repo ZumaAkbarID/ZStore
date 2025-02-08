2141d07621ced51512d8b5f5d62d29d491e4a699
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2025-02-09 00:21:19 +0700

    subdomain support

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,29 @@ use App\Http\Controllers\FrontEnd;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::group([
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
-], function () {
-    /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-    Route::get('/', [FrontEnd::class, 'index'])->name('frontend.index');
-
-    Route::get('/404', function () {
-        return abort('404');
-    })->name(
-        '404'
-    );
+Route::domain('zstore.localhost')->group(function () {
+    Route::group([
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ], function () {
+        /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+        Route::get('/', [FrontEnd::class, 'index'])->name('frontend.index');
+    });
 });
+
+Route::domain('account.zstore.localhost')->group(function () {
+    Route::group([
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ], function () {
+        Route::get('/', function () {
+            return "OK";
+        });
+    });
+});
+
+Route::get('/404', function () {
+    return abort('404');
+})->name(
+    '404'
+);
